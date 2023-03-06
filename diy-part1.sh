af91a11b9caca3577ea9b8da98933a8943c10f06
sagehou <sagehou@outlook.com>
2023-03-06 14:55:11 +0800

    first init.

---
+++ diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
+
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
